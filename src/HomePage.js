83416b6f28312921307a6267697b1fec541fbb48
erica-chiu <ecrainbow8@gmail.com>
2020-06-04 18:35:18 -0700

    Update home page

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 const HomePage = () => {
     return (
-        <div>
-            <header className="App-basic">
-                Welcome to Erica Chiu's home website.
-            </header>
+        <div className="home-page">
+            <div className="home-header">
+                Welcome to Erica Chiu's website!
+            </div>
+            <p>
+                I am an undergraduate at MIT studying computer science and mathematics, interested in machine learning. At MIT, I am involved with a few <a href="#/dance">dance</a> organizations, as well as <a href="https://techx.io">TechX</a>, an organization that hosts technology events.
+            </p>
+
+            <p>
+                This summer, I am excited to be interning at <a href="https://nuro.ai">Nuro</a> on the Machine Learning team. Last summer, I interned at <a href="https://www.salesforce.com/products/einstein/overview/">Salesforce Einstein</a> on the Modeling Team, working on AutoML features. 
+            </p>
         </div>
         
     );
