a5a0490c91dae90125f0395dd1de7e69e91106ab
erica-chiu <ecrainbow8@gmail.com>
2025-09-28 13:48:03 -0400

    Update home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,18 +8,11 @@ const HomePage = () => {
                 Welcome to Erica Chiu's website!
             </div>
             <p>
-                I am an undergraduate at MIT studying computer science and mathematics and 
-                interested in machine learning. At MIT, I am involved with a 
+                I am a graduate of MIT studying computer science and mathematics and 
+                interested in machine learning. At MIT, I was involved with a 
                 few <a href="#/dance">dance</a> organizations, as well 
                 as <a href="https://techx.io">TechX</a>, an organization 
                 that hosts technology events.
-            </p>
-
-            <p>
-                This summer, I am excited to be interning at <a href="https://nuro.ai">Nuro</a> on 
-                the Machine Learning team. Last summer, I interned 
-                at <a href="https://www.salesforce.com/products/einstein/overview/">Salesforce 
-                Einstein</a> on the Modeling Team, working on AutoML features. 
             </p>
         </div>
         
